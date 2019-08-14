1d8e54769e2043760bbc7104aa1b24ef2c56af2d
David Ubanyi <ubanyi@gmail.com>
2019-08-14 13:32:17 +0100

    adjusted webpack for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,42 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  mode: "development",
-  entry: "./src/app.js",
-  output: {
-    path: path.join(__dirname, "public"),
-    filename: "bundle.js"
-  },
-  module: {
-    rules: [
-      {
-        loader: "babel-loader",
-        test: /\.js$/,
-        exclude: /node_modules/
-      },
-      {
-        test: /\.s?css$/,
-        use: ["style-loader", "css-loader", "sass-loader"]
-      }
-    ]
-  },
-  devtool: "cheap-module-eval-source-map",
-  devServer: {
-    contentBase: path.join(__dirname, "public"),
-    historyApiFallback: true
-  }
+module.exports = env => {
+  const isProduction = env === "production";
+  ("styles.css");
+  return {
+    mode: "development",
+    entry: "./src/app.js",
+    output: {
+      path: path.join(__dirname, "public"),
+      filename: "bundle.js"
+    },
+    module: {
+      rules: [
+        {
+          loader: "babel-loader",
+          test: /\.js$/,
+          exclude: /node_modules/
+        },
+        {
+          test: /\.s?css$/,
+          use: [{loader: MiniCssExtractPlugin.loader,
+          options: {
+            publicPath: "../",
+            hmr:process.env.NODE_ENV !== 'production'
+          },},'css-loader', 'sass-loader']
+        }
+      ]
+    },
+    plugins: [new MiniCssExtractPlugin({
+      filename: 'styles.css',
+      chunkFilename: 'cssChunk.css',
+      ignoreOrder: false
+    })],
+    devtool: isProduction ? "source-map" : "cheap-module-eval-source-map",
+    devServer: {
+      contentBase: path.join(__dirname, "public"),
+      historyApiFallback: true
+    }
+  };
 };
